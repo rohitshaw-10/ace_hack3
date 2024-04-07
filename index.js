71a884841c8de5b67b7fdb10228a61b39d1995c3
rohitshaw-10 <rohitshaw3622@gmail.com>
2024-04-07 06:45:22 +0530

    ok

--- index.js
+++ index.js
@@ -20,7 +20,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin:"http://localhost:3000",
+        origin:["http://localhost:3000","https://deploy-mern-1whq.vercel.app"],
+        methods:["POST","GET","PUT"],
         credentials:true,
     })
 )
